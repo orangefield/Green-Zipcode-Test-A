8f1f262e40fc8e7cb22e0bad2ce3926c7e3cdacf
field <jsw_777@naver.com>
2022-06-09 22:59:46 +0900

    Code Refactoring 1

--- src/App.java
+++ src/App.java
@@ -1,45 +1,12 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.nio.charset.Charset;
-
 public class App {
 
-    // 파일 경로를 변수화
-    private static final String WORKSPACE = "C:\\green_workspace\\orangefield01\\zipcode-test-practice\\";
-    private static final String RESOURCE = WORKSPACE + "resource\\";
-
     public static void main(String[] args) throws Exception {
+        int result = TextToCsvFile.process("zipcode.txt", "zipcode.csv");
 
-        // 파일 읽기 과정
-        FileReader reader = new FileReader(RESOURCE + "zipcode.txt", Charset.forName("utf-8"));
-        BufferedReader br = new BufferedReader(reader);
-
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while ((line = br.readLine()) != null) {
-
-            // 구분자를 콤마로 변환
-            String csvline = line.replaceAll("\\^", ", ");
-            sb.append(csvline + "\n");
+        if (result == 1) {
+            System.out.println("csv 파일 생성에 성공했습니다");
+        } else {
+            System.out.println("csv 파일 생성을 실패했습니다");
         }
-        String resultLine = sb.toString();
-
-        // 버퍼를 열었으면 닫아야 한다
-        reader.close();
-        br.close();
-
-        // 파일 쓰기 과정
-        FileWriter writer = new FileWriter(RESOURCE + "zipcode.csv", Charset.forName("utf-8"));
-        BufferedWriter bw = new BufferedWriter(writer);
-
-        bw.write(resultLine);
-        bw.flush();
-
-        // 버퍼를 열었으면 닫아야 한다
-        writer.close();
-        bw.close();
-
     }
 }
---
+++ src/TextToCsvFile.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.charset.Charset;
+
+public class TextToCsvFile {
+
+    private static final String WORKSPACE = "C:\\green_workspace\\orangefield01\\zipcode-test-practice\\";
+    private static final String RESOURCE = WORKSPACE + "resource\\";
+
+    // path = 내 프로젝트 경로와 내부 resource 폴더가 내장되어 있습니다
+    // encType = UTF-8, EUC-KR 등을 입력합니다
+    private static String readFile(String path, String encType) {
+        try {
+
+            FileReader reader = new FileReader(RESOURCE + path, Charset.forName(encType));
+            BufferedReader br = new BufferedReader(reader);
+
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            reader.close();
+            br.close();
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            System.out.println("파일의 경로가 잘못 되었습니다");
+        }
+        return null;
+    }
+
+    // CSV 형태로 변환합니다
+    private static String convertCsv(String data, String seperator) {
+        return data.replaceAll("\\" + seperator, ", ");
+    }
+
+    private static int writeFile(String path, String encType, String csvStr) {
+        try {
+
+            FileWriter writer = new FileWriter(RESOURCE + path, Charset.forName(encType));
+            BufferedWriter bw = new BufferedWriter(writer);
+
+            bw.write(csvStr);
+            bw.flush();
+
+            writer.close();
+            bw.close();
+            return 1;
+
+        } catch (Exception e) {
+            System.out.println("파일 쓰기를 실패했습니다. 이유 : " + e.getMessage());
+        }
+        return -1;
+    }
+
+    public static int process(String readFilePath, String writeFilePath) {
+        String data = readFile(readFilePath, "UTF-8");
+        String csvData = convertCsv(data, "^");
+
+        return writeFile(writeFilePath, "UTF-8", csvData);
+    }
+}
